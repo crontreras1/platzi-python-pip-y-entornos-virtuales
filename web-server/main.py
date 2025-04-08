05e747031c3eac3ab2e4e704ffa60b6f79cc892e
Cristian <crontreras1@gmail.com>
2025-04-08 11:06:12 -0500

    class 14. web server with fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/') # Decorador
+def get_list ():
+  return [1, 2, 3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list ():
+  return '''
+  <h1>Esto es un título</h1>
+  <p>Esto es un párrafo</p>
+  '''
 
 def run ():
   store.get_categories()
